aa20bdb4e410de270209875f34e7b92554233570
Donghyeon Lee <ehdgus001928@gmail.com>
2023-05-01 21:20:04 +0900

    feat: Add digit condition
    Get number when user input is digit.
    if not, receive input again.

--- input_number.py
+++ input_number.py
@@ -2,7 +2,7 @@
 def input_num():
     while True:
         user_input = input("Input 4 digits.\n")
-        if len(set(user_input)) == 4:
+        if user_input.isdigit() and len(set(user_input)) == 4:
             break
 
     return user_input
