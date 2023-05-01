4d55c2b6e4bc71df0981bcba32b9ebd5a1fffcf5
charmeee <mandugohigh@gmail.com>
2023-05-01 20:16:29 +0900

    feat: 여러함수를 이용하여 체크하는 반복문 추가

--- main.py
+++ main.py
@@ -6,8 +6,30 @@ logo = """
 | (__ | (_) | \ V  V / \__ \ | (_>  < | |_) || |_| || || |\__\ 
  \___| \___/   \_/\_/  |___/  \___/\/ |_.__/  \__,_||_||_||___/
                             
-"""
+""";
 print(logo);
+
+randomNum = getRandomNum();
+
+while True:
+    # getInput
+    inputNum = [];
+    for i in range(4):
+        tmp=int(input(i+1+'번째 수를 입력해주세요'));
+        inputNum.append(tmp);
+    # checkInput
+    bulls,cows = checkInput();
+    print('bulls :' + bulls);
+    print('cows :' + cows);
+    if bulls == 4: 
+        print('정답입니다!');
+        break;
+
+
+
+
+    
+
 
 
 
