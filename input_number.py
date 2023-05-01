97eb35f33562ee6b1257353eef84c62ed93cca57
Donghyeon Lee <ehdgus001928@gmail.com>
2023-05-01 20:22:27 +0900

    feat: Add duplicate condition
    Get number when there is no duplicated number.

--- input_number.py
+++ input_number.py
@@ -2,7 +2,7 @@
 def input_num():
     while True:
         user_input = input("Input 4 digits.\n")
-        if len(user_input) == 4:
+        if len(set(user_input)) == 4:
             break
 
     return user_input
