1b331303095a3484439cf42a8bd7fefe42ecfe97
[Jiho-Park-0] <jihodayo@gmail.com>
2023-05-01 20:16:17 +0900

    feat: Modify count function.
    카운트 하는 과정에서 오류 수정

--- count.py
+++ count.py
@@ -4,8 +4,7 @@ def count_bulls_cows(player_num, answer):
     for i in range(len(player_num)):
         if player_num[i] == answer[i]:
             bulls += 1
-        elif player_num[i] in answer[i]:
+        elif player_num[i] in answer:
             cows += 1
-
     return bulls, cows
 
